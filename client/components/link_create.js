85caa3cdad505b42b820e59b4f68a6904f6d1677
Mike Peiman <mikepeiman@gmail.com>
2016-08-14 14:37:41 -0400

    removed meteor package autopublish. developing link validation and error callbacks

--- client/components/link_create.js
+++ client/components/link_create.js
@@ -1,10 +1,22 @@
 import React, { Component } from 'react';
 
 class LinkCreate extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = { error: '' };
+	}
 	handleSubmit(event) {
 		event.preventDefault();
 
-		Meteor.call('links.insert', this.refs.input.value);
+		Meteor.call('links.insert', this.refs.input.value, (error) => {
+			if (error) {
+				this.setState({ error: 'Enter a valid URL' });
+				} else {
+					this.setState({ error: '' });
+					this.refs.input.value = '';
+				}
+		});
 
 	}
 	render() {
@@ -14,6 +26,7 @@ class LinkCreate extends Component {
 					<label>Link to shorten</label>
 					<input ref="input" className="form-control" />
 				</div>
+				<div className="text-danger">{this.state.error}</div>
 				<button className="btn btn-primary">Shorten!</button>
 			</form>
 		);
--- imports/collections/links.js
+++ imports/collections/links.js
@@ -1,8 +1,15 @@
 import { Mongo } from 'meteor/mongo';
+import validUrl from 'valid-url';
+import { check, Match } from 'meteor/check';
 
 Meteor.methods({
 	'links.insert': function(url) {
-	console.log('attempting to save', url);
+		console.log(validUrl.isUri(url));
+		check(url, Match.Where(url => validUrl.isUri(url)));
+
+		// We're ready to save the URL
+		const token = Math.random().toString(36).slice(-5);
+		Links.insert({ url, token, clicks: 0 });
 	}
 });
 
