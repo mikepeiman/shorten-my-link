f9dad70b7280f3bee4fdcbe3e99e9efe0e34a337
Mike Peiman <mikepeiman@gmail.com>
2016-08-14 16:49:27 -0400

    click counter increments to the database. redirects functions. app complete.

--- server/main.js
+++ server/main.js
@@ -1,6 +1,7 @@
 import { Meteor } from 'meteor/meteor';
 import { Links } from '../imports/collections/links';
 import { WebApp } from 'meteor/webapp';
+import ConnectRoute from 'connect-route';
 
 Meteor.startup(() => {
   Meteor.publish('links', function() {
@@ -8,4 +9,31 @@ Meteor.startup(() => {
   });
 });
 
-WebApp.connectHandlers.use(req => console.log(req));
+// executed whenever a user visits with a route like
+// 'localhost:3000/abcd'
+function onRoute(req, res, next) {
+// take the token out of the url and try to find a
+// matching link in the Links collection
+	const link = Links.findOne({ token: req.params.token });
+
+	if (link) {
+	// If we find a link object, redirect the user to the long URL
+
+	// why not link.clicks += 1;
+		Links.update(link, { $inc: { clicks: 1 }});
+		res.writeHead(307, { 'Location': link.url });
+		res.end();
+	} else {
+	// If we don't find a link object, send the user to our normal React app
+		next();
+	}
+}
+// localhost:3000/  NO MATCH
+// localhost:3000/books/mastery  NO MATCH
+// localhost:3000/abcd  WILL MATCH
+
+const middleware = ConnectRoute(function(router) {
+	router.get('/:token', onRoute);
+});
+
+WebApp.connectHandlers.use(middleware);
