bf0ecdd0d3b038b274f7daa58cc4928f80695b2f
Mike Peiman <mikepeiman@gmail.com>
2016-08-14 15:53:52 -0400

    installed several packages, now dealing with http requests and routing

--- server/main.js
+++ server/main.js
@@ -1,6 +1,11 @@
 import { Meteor } from 'meteor/meteor';
 import { Links } from '../imports/collections/links';
+import { WebApp } from 'meteor/webapp';
 
 Meteor.startup(() => {
-  // code to run on server at startup
+  Meteor.publish('links', function() {
+  	return Links.find({});
+  });
 });
+
+WebApp.connectHandlers.use(req => console.log(req));
